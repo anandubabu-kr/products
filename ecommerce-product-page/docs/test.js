d5c0fbe8ab707789ea898af8c1c78fac3787b60b
AnanduBabucj7 <anandubabu.sd@gmail.com>
2022-07-17 15:26:13 +0530

    Updtaed folder hierarchy

---
+++ ecommerce-product-page/docs/test.js
@@ -0,0 +1,6 @@
+let calcAverage = (res1, res2, res3) =>  (res1 + res2 + res3) / 3 
+let dolphinsScore = calcAverage(42 , 42 , 12) 
+let koalasScore = calcAverage(321 , 221 , 324) 
+
+
+console.log(dolphinsScore, koalasScore)
